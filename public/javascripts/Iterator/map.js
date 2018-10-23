b3c86fe8a5f5e475060d110c4a9f7afd7bc40e86
yonghai.yang <yangyonghai@wusongtech.com>
2018-10-23 11:46:32 +0800

    Map over :banana:

--- public/javascripts/Iterator/map.js
+++ public/javascripts/Iterator/map.js
@@ -15,9 +15,11 @@ const obj = {};
 obj[o] = 'content'; // ==>  obj['[object Object]'] = 'content';
 obj[l] = 'fuck that!';
 
-console.log(obj, obj[l], obj[o]); //
+console.log(obj, obj[l], obj[o]); //{ '[object Object]': 'fuck that!' } 'fuck that!' 'fuck that!'
 
 // 由上可知，Map的键实际上是跟内存地址绑定的，只要内存地址不一样，就视为两个键，这就解决了同名属性碰撞（clash）的问题，我们扩展别人的库的时候，如果使用对象作为键名，就不用担心自己的属性名和原作者的属性同名。
 
-
+console.log(m.has(o)); // true
+m.delete(o);
+console.log(m.has(o)); // false
 
